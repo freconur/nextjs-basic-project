2324b346bda80c68325c7330054b02a054965f86
frecodev <freconur@gmail.com>
2023-04-12 12:56:16 -0500

    agregando icon

--- components/SidebarBts/SideBar.tsx
+++ components/SidebarBts/SideBar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import React, { useEffect, useReducer, useState } from 'react'
 import { getBtsCategories } from 'reducer/Bts'
 import { ProductsReducer, productInitialState } from 'reducer/Products.reducer'
-
+import { RiArrowDownSLine } from "react-icons/ri";
 const SideBar = () => {
 
   const [state, dispatch] = useReducer(ProductsReducer, productInitialState)
@@ -16,8 +16,9 @@ const SideBar = () => {
     <>
       <div className='sticky top-0 right-0 z-20'>
         <div onClick={() => setShowCategories(!showCategories)} className='relative cursor-pointer justify-center flex items-center  text-center w-full bg-blanco-cool h-[60px] drop-shadow-md'>
-          <p className={`relative z-20 text-xl leading-[3.25rem] w-full h-full bg-blanco-cool  capitalize font-bold ${showCategories ? "bg-violet-300 duration-300 transition text-blanco-cool" : "text-texto"}`}>
+          <p className={`relative z-20 justify-center items-center flex text-xl leading-[3.25rem] w-full h-full bg-blanco-cool  capitalize font-bold ${showCategories ? "bg-violet-300 duration-300 transition text-blanco-cool" : "text-texto"}`}>
             categorias
+            <RiArrowDownSLine className={` mt-1 duration-300 transition ${showCategories && "-rotate-90 duration-300 transition"}`}/>
           </p>
 
           <ul className={`overflow-hidden fixed z-10 -translate-y-[200%] duration-500 transition rounded-b-xl bg-blanco-cool shadow-xl first-letter w-full ${showCategories && "translate-y-[235px] duration-500 transition "}`}>
