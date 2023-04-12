b656d10ad0834915e668fb6128d3a213294358bb
frecodev <freconur@gmail.com>
2023-04-12 10:40:25 -0500

    verificando css

--- components/ProductsCard/ProductsCard.tsx
+++ components/ProductsCard/ProductsCard.tsx
@@ -9,18 +9,18 @@ const ProductsCard = ({ posterBts }: Props) => {
 		<ul className='m-3 grid grid-cols-1 gap-4'>
 			{posterBts?.map(item => {
 				return (
-					<li className='grid grid-cols-gridCardProduct rounded-xl  bg-blanco-cool shadow-md overflow-hidden'>
+					<li key={item.id} className='grid grid-cols-gridCardProduct rounded-xl  bg-blanco-cool shadow-md overflow-hidden'>
 						<img className='w-full' src={item.image} alt={item.name} />
 						<div className='grid p-2 ml-2'>
-							<h4 className='text-texto-title font-semibold uppercase'>
+							<h4 className='text-texto-title text-lg font-semibold uppercase'>
 								{item.marca}
 							</h4>
-							<h3 className='text-2xl font-medium capitalize'>
+							<h3 className='text-xl font-medium capitalize'>
 								{item.name}
 							</h3>
 							<div className='flex justify-between'>
-								<p>S/{item.price}</p>
-								<div className='w-10 h-8 text-center leading-[28px] bg-fuxia rounded-md text-blanco-cool font-medium'><span>ver</span></div>
+								<p className='text-lg'>S/{item.price}</p>
+								<div className='w-10 h-8 text-center leading-[28px] bg-fuxia rounded-md text-blanco-cool font-medium text-lg'><span>ver</span></div>
 							</div>
 						</div>
 					</li>
