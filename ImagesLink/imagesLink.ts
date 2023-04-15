470117648edd8e925e500596942804ab391b73bd
frecodev <freconur@gmail.com>
2023-04-15 12:11:21 -0500

    agregando la pagina de tazas

--- ImagesLink/imagesLink.ts
+++ ImagesLink/imagesLink.ts
@@ -30,3 +30,13 @@ export const IMAGES_KAWAII = {
     section:"https://firebasestorage.googleapis.com/v0/b/apiwaliky.appspot.com/o/kawaii%2Fbanner-kawaii%2Fkawaii-banner.png?alt=media&token=bb75fd4a-8fed-4f73-a2aa-84b81b0c8808"
   }
 }
+export const TAZAS_SUBLIMADOS = {
+  personalizados:{
+    url:"https://firebasestorage.googleapis.com/v0/b/apiwaliky.appspot.com/o/sublimados%2Ftazas-sublimados-custom%2Ftazas-sublimados-custom.jpg?alt=media&token=f37a06af-5ab7-40d7-8454-9824d72120d7",
+    name:"plantillas"
+  },
+  diseno:{
+    url:"https://firebasestorage.googleapis.com/v0/b/apiwaliky.appspot.com/o/sublimados%2Ftazas-sublimados-custom%2Ftaza-sublimadas-diseno.jpg?alt=media&token=966841cf-5fd9-459b-9e37-bf5c5c2465bd",
+    name:"diseno"
+  },
+}
