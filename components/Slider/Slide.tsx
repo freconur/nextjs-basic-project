cd9632cbbb39fad50c8cc2839c41ae32bdd8156e
frecodev <freconur@gmail.com>
2023-04-18 22:14:38 -0500

    agregando window width

--- components/Slider/Slide.tsx
+++ components/Slider/Slide.tsx
@@ -3,41 +3,52 @@ import { IMAGE_SLIDER_SM, IMAGE_SLIDER_XS } from "./ArrayImageSlider";
 import Carrousel from "./Carrousel";
 import Image from "next/image";
 import { imageBlur } from "ImagesLink/blurImage";
+import { useEffect, useState } from "react";
 
 const Slide = () => {
+  const [size, setSize] = useState<number>(0);
+  useEffect(() => {
+    const handleResize = () => {
+      setSize(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+  }, []);
   return (
     <>
-      <main className="xs:hidden relative">
-        <Carrousel autoSlide={true}>
-          {IMAGE_SLIDER_XS.map((url, index) => (
-            <Image
-              key={index}
-              src={url.urlImage}
-              width='500'
-              height='500'
-              alt={url.name}
-              // priority
-              blurDataURL={imageBlur}
-              placeholder="blur"
+      {size < 540 ?
+        <main className="xs:hidden relative">
+          <Carrousel autoSlide={true}>
+            {IMAGE_SLIDER_XS.map((url, index) => (
+              <Image
+                key={index}
+                src={url.urlImage}
+                width='500'
+                height='500'
+                alt={url.name}
+                priority
+                blurDataURL={imageBlur}
+                placeholder="blur"
               />
-              ))}
-        </Carrousel>
-      </main>
-      <main className="hidden xs:block">
-        <Carrousel autoSlide={true}>
-          {IMAGE_SLIDER_SM.map((url, index) => (
-            <Image
-              key={index}
-              src={url.urlImage}
-              width={1900}
-              height={650}
-              alt={url.name}
-              priority
+            ))}
+          </Carrousel>
+        </main>
+        :
+        <main className="hidden xs:block">
+          <Carrousel autoSlide={true}>
+            {IMAGE_SLIDER_SM.map((url, index) => (
+              <Image
+                key={index}
+                src={url.urlImage}
+                width={1900}
+                height={650}
+                alt={url.name}
+                priority
               // placeholder="blur"
-            />
-          ))}
-        </Carrousel>
-      </main>
+              />
+            ))}
+          </Carrousel>
+        </main>
+      }
     </>
   );
 };
