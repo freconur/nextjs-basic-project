2afc95401ac129763d59952f51aea92c3295ea11
frecodev <freconur@gmail.com>
2023-04-19 21:46:26 -0500

    agregando submenu al menu movil

--- components/MenuMobile/MenuMobile.tsx
+++ components/MenuMobile/MenuMobile.tsx
@@ -1,26 +1,32 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 interface PropsMenuMobile {
   showModalNavbar: boolean
   // setShowModalNavbar: React.Dispatch<React.SetStateAction<boolean>>;
 }
 const MenuMobile = ({ showModalNavbar }: PropsMenuMobile) => {
+  const [showSubmenu, setShowSubmenu] = useState<boolean>(false)
   return (
     // <ul className={`fixed rounded-lg translate-y-[-200%]  bg-verde botton-20 right-5 z-50 ${showModalNavbar && "translate-y-0 duration-300 transition"}`}> 
-    <ul className={`fixed rounded-lg bg-verde bottom-20 right-5 z-50`}>
+    <ul className={`overflow-hidden fixed rounded-lg bg-verde bottom-20 right-5 z-50`}>
       <li>
-        <Link href="/" legacyBehavior>
-          <a className="text-blanco-cool hover:bg-cian font-semibold capitalize block leading-10 px-3 ">productos</a>
+        <Link href="" className="text-blanco-cool hover:bg-cian text-xl font-semibold capitalize block leading-10 p-3  text-center">
+          productos
+        </Link>
+        <ul className='bg-blanco-cool'>
+          <li className='text-lg font-semibold capitalize block leading-10 p-1 text-center text-texto-title hover:bg-violeta hover:text-blanco-cool cursor-pointer duration-300 transition'>biombos</li>
+          <li className='text-lg font-semibold capitalize block leading-10 p-1 text-center text-texto-title hover:bg-violeta hover:text-blanco-cool cursor-pointer duration-300 transition'>Bts</li>
+          <li className='text-lg font-semibold capitalize block leading-10 p-1 text-center text-texto-title hover:bg-violeta hover:text-blanco-cool cursor-pointer duration-300 transition'>sublimados</li>
+        </ul>
+      </li>
+      <li>
+        <Link href="/contactanos" className="text-blanco-cool hover:bg-cian text-xl font-semibold capitalize block leading-10 p-3 text-center">
+          contactanos
         </Link>
       </li>
       <li>
-        <Link href="/" legacyBehavior>
-          <a className="text-blanco-cool hover:bg-cian font-semibold capitalize block leading-10 px-3">contactanos</a>
-        </Link>
-      </li>
-      <li>
-        <Link href="/" legacyBehavior>
-          <a className="text-blanco-cool hover:bg-cian font-semibold capitalize block leading-10 px-3">nosotros</a>
+        <Link href="/nosotros" className="text-blanco-cool hover:bg-cian text-xl font-semibold capitalize block leading-10 p-3 text-center">
+          nosotros
         </Link>
       </li>
     </ul>
