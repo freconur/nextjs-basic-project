0abffdb1af40bd6c71201a7c03491f96dd54b141
frecodev <freconur@gmail.com>
2023-04-21 12:53:15 -0500

    add one more url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -7,6 +7,7 @@ module.exports = {
     robotsTxtOptions: {
         additionalSitemaps: [
             `${siteUrl}/sitemap-0.xml`,
+            `${siteUrlSecond}/sitemap.xml`,
             `${siteUrlSecond}/sitemap-0.xml`,
             `${siteUrlSecond}/server-sitemap.xml`,
             `${siteUrl}/server-sitemap.xml`,
